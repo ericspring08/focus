e055e9f6342b8a02f23d7f5e279ddfcd9cfb2884
theric <69996843+ericspring08@users.noreply.github.com>
2021-05-16 08:36:02 -0500

    server

--- api/main.py
+++ api/main.py
@@ -1,14 +1,24 @@
-import json
 from flask import Flask, jsonify, request
+from ml import final_file
+from PIL import Image, ImageFile
+from io import BytesIO
+import base64
+import numpy
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 app = Flask(__name__)
 @app.route('/', methods=['GET'])
 
 def query():
-    image1 = request.args.get('image1')
-    image2 = request.args.get('image2')
-    image3 = request.args.get('image3')
-    image4 = request.args.get('image4')
-    image5 = request.args.get('image5')
-
-    return(image1 + " " + image2)
+    final_model = final_file.get_model("ml/hackethernet.h5")
+    image = request.args.get('image')
+    base64Data = image + "=="
+    img = Image.open(BytesIO(base64.b64decode(base64Data)))
+    imgArray = numpy.array(img.getdata()).reshape(img.size[0], img.size[1], 3)
+    print(imgArray)
+    print(imgArray.size)
+    response = final_model.predict(imgArray)
+    return jsonify (
+        _class = str(response[0]),
+        _prob = float(response[1]),
+    )
 app.run()
